bd091405c4f97c1693942fce2d15c20258abe5e6
Engy114 <140180137+Engy114@users.noreply.github.com>
2024-10-20 21:10:52 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Retrieve the full description of the 'books' table
+-- Retrieve the full description of the 'Books' table (with correct case)
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'books'
+WHERE TABLE_NAME = 'Books'  -- Ensure the table name is capitalized
 AND TABLE_SCHEMA = 'alx_book_store';
